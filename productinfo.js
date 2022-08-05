9f672399928ca5537a662bb1b07b34f1d0dabea0
christale <christalebu@gmail.com>
2022-08-05 10:19:52 -0700

    productinfo

--- productinfo.js
+++
@@ -1,7 +0,0 @@
-// const infoBtn = document.getElementById('product-info')
-
-// infoBtn.addEventListener("click", function onOpen(){
-//     const listElement= document.querySelector('#List-of-Elements')
-//     listElement.showModal();
-//     console.log("clicked");
-// })
